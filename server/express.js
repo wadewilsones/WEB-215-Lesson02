9427a649bda3ac8f0e70ac60ea8be42a1ada7a18
wadewilsones <uladaharanina@gmail.com>
2022-05-23 12:50:50 -0400

    packages werer added

---
+++ server/express.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+import helmet from 'helmet'
+
+const app = express();
+
+
+/*Express Config*/
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(compress());
+app.use(helmet());
+app.use(cors());
+
+export default app;
---
+++ server/server.js
@@ -0,0 +1,9 @@
+import config from "../config/config";
+import app from './express';
+
+app.listen(config.port, (err) => {
+    if(err) {
+        console.log(err)
+    }
+    console.info('Server started om port %s.', config.port)
+})
