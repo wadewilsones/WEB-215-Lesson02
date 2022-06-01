c57234adb340398cfff7451d1f3ed980115aaff3
Ulada <uladaharanina@gmail.com>
2022-06-01 18:56:08 -0400

    No errors

--- client/core/Home.js
+++ client/core/Home.js
@@ -4,7 +4,7 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
-import unicornbikeImg from './../assets/images/unicornbike.jpg'
+import unicornbikeImg from './../assets/images/uni.jpg'
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -39,7 +39,7 @@ export default function Home(){
             Home Page
           </Typography>
           <CardMedia className={classes.media} image={unicornbikeImg} title="Unicorn Bicycle"/>
-          <Typography variant="body2" component="p" className={classes.credit} color="textSecondary">Photo by <a href="https://unsplash.com/@boudewijn_huysmans" target="_blank" rel="noopener noreferrer">Boudewijn Huysmans</a> on Unsplash</Typography>
+          <Typography variant="body2" component="p" className={classes.credit} color="textSecondary">hello</Typography>
           <CardContent>
             <Typography variant="body1" component="p">
               Welcome to the MERN Skeleton home page.
